da630bd254afdf1cf9a8e150f3d732bfafb16de6
Muhammad Farouq Rafie <104681502+inyund@users.noreply.github.com>
2026-07-23 00:03:42 +0700

    add/update 01-variables-and-data-types/mutability-check.js

---
+++ 01-variables-and-data-types/mutability-check.js
@@ -0,0 +1,12 @@
+// Tuliskan analisis dan hasil percobaan Anda di sini
+const dua = "defaultvaluedua";
+dua[2] = "x";
+console.log(dua);
+
+const tiga = "defaultvaluetiga";
+console.log(tiga);
+
+const empat = [1, 9, 6];
+console.log(empat);
+empat[1] = "valuedilistpertama";
+console.log(empat);
