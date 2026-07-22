aa238f881087a63e7f79cce9f0e2bb274fcb0d44
Muhammad Farouq Rafie <104681502+inyund@users.noreply.github.com>
2026-07-23 00:58:04 +0700

    delete 02-variables-and-data-types/mutability-check.js

--- 02-variables-and-data-types/mutability-check.js
+++
@@ -1,8 +0,0 @@
-// Tulis analisis mutabilitas Anda di sini
-const dua = "defaultvaluedua";
-dua[2] = "x";
-console.log(dua);
-
-const empat = [1, 9, 6];
-empat[1] = "valuedilistpertama";
-console.log(empat);
