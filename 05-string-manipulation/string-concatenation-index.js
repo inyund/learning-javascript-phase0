f889f2acca90eb2782b62d71383698253554cc1d
Muhammad Farouq Rafie <104681502+inyund@users.noreply.github.com>
2026-07-22 23:59:58 +0700

    add 05-string-manipulation/string-concatenation-index.js

---
+++ 05-string-manipulation/string-concatenation-index.js
@@ -0,0 +1,18 @@
+var word = 'JavaScript';
+var second = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = 'I';
+var sixth = 'love';
+var seventh = 'it!';
+
+// 1. Penggabungan kata
+// ...
+
+var word2 = 'wow JavaScript is so cool';
+// 2. Akses via index
+// ...
+
+var word3 = 'wow JavaScript is so cool';
+// 3. Substring & length
+// ...
