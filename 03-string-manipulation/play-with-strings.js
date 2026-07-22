0f86a9cf8e06fb6a01b8900bafb54560234e9a16
Muhammad Farouq Rafie <104681502+inyund@users.noreply.github.com>
2026-07-23 00:58:17 +0700

    delete 03-string-manipulation/play-with-strings.js

--- 03-string-manipulation/play-with-strings.js
+++
@@ -1,9 +0,0 @@
-var word = 'JavaScript';
-var second = 'is';
-var third = 'awesome';
-
-console.log(word + ' ' + second + ' ' + third);
-
-var word2 = 'wow JavaScript is so cool';
-var exampleFirstWord = word2[0] + word2[1] + word2[2];
-console.log('First Word: ' + exampleFirstWord);
